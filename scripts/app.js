f7c567c62140e2f4b39a1191f4fc9bddcdc47a82
Ariel Nykanen <arielnykan3n@gmail.com>
2018-10-05 11:37:07 +0300

    added break time indicator

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@
     let startTimeTxt = document.getElementById('startTimeTxt');
     let runningTimeTxt = document.getElementById('runningTimeTxt');
     let totalTimeTxt = document.getElementById('totalTimeTxt');
+    let breakTimeTxt = document.getElementById('breakTime');
 
     let startB = document.getElementById('startB').addEventListener('click',function(e){
         e.preventDefault();
@@ -20,7 +21,13 @@
         
     });
 
-    let startTime, totalTime, runner, watchOn = false, Startdate,  secounds = 0, minutes= 0, hours = 0, miliSeconds;
+    let startTime, totalTime, runner, watchOn = false, Startdate,  secounds = 0, minutes= 0, hours = 0, totalBreaks = 0;
+    
+    let breakTime = {
+        s : 0,
+        m : 0,
+        h : 0
+    }
 
     this.setDate = function(){
         let currentDateTime = new Date();
@@ -30,6 +37,7 @@
         if(secounds < 0.10) 
         Startdate = `${h}:${m}:${s}`;
     }
+
 
     this.start_End_runing = function(onOff){
         if(onOff === true){
@@ -62,13 +70,37 @@
 
     this.stop_watch = function(){
         clearInterval(this.runner);
+            this.countBreakTime();
             totalTimeTxt.innerHTML = totalTime;
+            breakTimeTxt.innerHTML = totalBreaks;
             watchOn = false;
+    }
+
+    this.countBreakTime = function(){
+        
+        
+        if(!watchOn === false ){
+        let count = setInterval(function(){
+            breakTime.s++;
+            if(breakTime.s > 59){
+                breakTime.m++;
+                breakTime.s = 0;
+            }
+            if(breakTime.m === 59 && breakTime.s === 59)
+                breakTime.h++; 
+            if(watchOn === true){
+                clearInterval(count);
+            }
+            totalBreaks = `${breakTime.h}:${breakTime.m}:${breakTime.s}`;
+        },1000);
+    }
+        
     }
 
     this.reset_watch = function(){
 
         start_End_runing(false);
+        totalBreaks = 0;
         secounds = 0;
         totalTime = null;
         startTimeTxt.innerHTML = '';
@@ -81,7 +113,16 @@
      this, 'watchOn', this, 'startTimeTxt',
       this, 'secounds', this, 'totalTimeTxt',
       this, 'runningTimeTxt',this, 'miliSeconds',
-      this, 'minutes', this, 'hours', this, 'totaltime',{
+      this, 'minutes', this, 'hours', this, 'totaltime',
+      this, 'totalBreaks',this, 'breakTimeTxt',{
+        get: function(){
+            return breakTimeTxt;
+        },
+
+        get: function(){
+            return totalBreaks;
+        },
+
         get: function(){
             return totaltime;
         },
